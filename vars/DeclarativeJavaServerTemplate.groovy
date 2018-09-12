a9f532125dffa6142686b7ad6fb0318c15ec4fcb
jkrust86 <43008839+jkrust86@users.noreply.github.com>
2018-09-12 15:41:37 -0500

    Update DeclarativeJavaServerTemplate.groovy

--- vars/DeclarativeJavaServerTemplate.groovy
+++ vars/DeclarativeJavaServerTemplate.groovy
@@ -1,3 +1,8 @@
+def call(body){
+def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
 pipeline {
     agent {
         label 'master'
